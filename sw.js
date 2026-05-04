e6b76c6ee0dcba8643367f59de677d080b4d45b3
amamiyafu <fu2026amamiya@gmail.com>
2026-05-05 03:11:49 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,73 @@ self.addEventListener("install", (event) => {
       );
     })
   );
+  self.skipWaiting();const CACHE_NAME = "medication-memo-cache-v8";
+const APP_SHELL = [
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(
+        APP_SHELL.map((url) => new Request(url, { cache: "reload" }))
+      );
+    })
+  );
   self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put("./index.html", responseCopy);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        const responseCopy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseCopy);
+        });
+        return response;
+      });
+    })
+  );
+});
 });
 
 self.addEventListener("activate", (event) => {
